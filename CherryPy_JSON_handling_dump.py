9e2647aad74373d491ffae047b8c79421ae81d30
Kamran Mosleh Khorrami <139634188+KamranMosleh@users.noreply.github.com>
2025-02-08 15:42:03 +0100

    initial commit for json dump

---
+++ CherryPy_JSON_handling_dump.py
@@ -0,0 +1,24 @@
+import json
+import os  # to go beyond the Python environment for file path handling, and for file management.
+# e.g.: create or delete files or folders, rename files, and even run other programs.
+
+# Get the path of the current script
+script_path = os.path.abspath(__file__)
+# Get the current directory of the script
+script_dir = os.path.dirname(script_path)
+json_file_name = 'CherryPy_JSON_handling_dump.json'
+# Define the path of the JSON file in the current directory
+json_file_path = os.path.join(script_dir, json_file_name)
+
+# create a JSON file and write data to it
+data = {'temperature': 25.5, 'humidity': 60} # data to be written to JSON file (dictionary)
+# Save the JSON data to the file
+with open(json_file_path, 'a') as json_file:
+    json.dump(data, json_file, indent=4)  # indent for pretty printing (optional)
+
+
+# CherryPy_JSON_handling_dump.json will contain:
+# {
+#     "temperature": 25.5,
+#     "humidity": 60
+# }
